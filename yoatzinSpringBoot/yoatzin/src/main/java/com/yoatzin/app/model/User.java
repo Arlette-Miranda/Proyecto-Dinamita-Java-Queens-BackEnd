f92d6a4aaa8e6d0b7f66e72acab966ae17783d40
Jazmín CI <jazzpchan.93@gmail.com>
2024-08-05 21:15:05 -0500

    Se agrego la llave foranea de user y product

--- yoatzinSpringBoot/yoatzin/src/main/java/com/yoatzin/app/model/User.java
+++ yoatzinSpringBoot/yoatzin/src/main/java/com/yoatzin/app/model/User.java
@@ -18,7 +18,7 @@ import jakarta.persistence.Table;
 public class User {
 	@Id
 	@GeneratedValue(strategy=GenerationType.IDENTITY)
-	private Long id;
+	private Long id; 
 	
 	@ManyToOne
 	@JoinColumn(name= "fk_id_privilege")
